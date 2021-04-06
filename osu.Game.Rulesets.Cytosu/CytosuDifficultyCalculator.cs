d1f1d6a29398ec7561e54d2ae308af6738a15b6c
Ganendra Afrasya <gsculerlor@hotmail.com>
2021-04-07 01:22:13 +0700

    bump osu version
    my new pc is here so I should be able to check this project again

--- osu.Game.Rulesets.Cytosu/CytosuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Cytosu/CytosuDifficultyCalculator.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Beatmaps;
@@ -25,6 +26,9 @@ namespace osu.Game.Rulesets.Cytosu
 
         protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, double clockRate) => Enumerable.Empty<DifficultyHitObject>();
 
-        protected override Skill[] CreateSkills(IBeatmap beatmap) => System.Array.Empty<Skill>();
+        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods)
+        {
+            return Array.Empty<Skill>();
+        }
     }
 }
